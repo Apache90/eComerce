068e3b492d20d076b51dc58c1e56e6c38cf90869
Emir Segovia <95300946+Apache90@users.noreply.github.com>
2023-03-24 16:47:38 -0300

    se soluciono problema de direccion con archivos estaticos

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const products = [
+  {
+    id: 1,
+    name: "name",
+    image: "",
+    price: 32000,
+    size: 42,
+    stock: 5,
+  },
+  {
+    id: 2,
+    name: "name",
+    image: "",
+    price: 32000,
+    size: 42,
+    stock: 5,
+  },
+  {
+    id: 3,
+    name: "name",
+    image: "",
+    price: 32000,
+    size: 42,
+    stock: 5,
+  },
+];
+
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
+
+app.use("/", express.static("frontEnd"));
+app.use("/", express.static("assets"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
