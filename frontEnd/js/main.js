55d5a2b912903bc5f641c9dfb2b089dd29b68fc7
Emir Segovia <95300946+Apache90@users.noreply.github.com>
2023-03-31 21:14:10 -0300

    .

--- frontEnd/js/main.js
+++ frontEnd/js/main.js
@@ -1,13 +1,13 @@
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId);
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if (toggle && nav) {
-        toggle.addEventListener("click", () => {
-            nav.classList.toggle("show");
-        });
-    }
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
 };
 showMenu("nav-toggle", "nav-menu");
 /*===== REMOVE MENU =====*/
@@ -15,7 +15,7 @@ const navLink = document.querySelectorAll(".nav_link");
 navMenu = document.getElementById("nav-menu");
 
 function linkAction() {
-    navMenu.classList.remove("show");
+  navMenu.classList.remove("show");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
@@ -25,59 +25,93 @@ const sections = document.querySelectorAll("section[id]");
 window.addEventListener("scroll", scrollActive);
 
 function scrollActive() {
-    const scrollY = window.pageYOffset;
+  const scrollY = window.pageYOffset;
 
-    sections.forEach((current) => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute("id");
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector(".nav_menu a[href*=" + sectionId + "]").classList.add("active");
-        }else {
-            document.querySelector(".nav_menu a[href*=" + sectionId + "]").classList.remove("active");
-        }
-    });
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
 }
 
 /*===== CHANGE COLOR HEADER =====*/
-window.onscroll = ()=>{
-    const nav = document.getElementById('header')
-    if(this.scrollY >=200) nav.classList.add('scroll-header');
-    else nav.classList.remove('scroll-header')
-}
+window.onscroll = () => {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+};
 /*===== PAY PRODUCTS =====*/
-let products = [];
+let productList = [];
+let carrito = [];
 let total = 0;
 
-function add(product,price){
-    console.log(product,price);
-    products.push(product);
-    total = total + price;
-    document.getElementById('pay').innerHTML = `$ ${total}`
-}
-function pay(){
-    window.alert(products.join(", \n"));
+function add(productId, price) {
+  const product = productList.find((p) => p.id === productId);
+  product.stock--;
+
+  console.log(productId, price);
+  carrito.push(productId);
+  total = total + price;
+  document.getElementById("checkout").innerHTML = `Pagar $${total}`;
+  displayProducts();
 }
 
-/*===== SHOW PRODUCTS =====*/
-function displayProducts(productlist){
-    let productsHTML = '';
-    productlist.forEach(element => {
-        productsHTML +=
-        `<article class="sneaker">
-            <img src="${element.image}" alt="" class="sneaker_img">
-            <span class="sneaker_name">${element.name}</span>
-            <span class="sneaker_price">$${element.price}</span>
-            <a class="button-light" onclick="add(${element.id},${element.price})">Agregar <i class='bx bxs-arrow-from-left button-icon'></i></a>
-        </article>`
-    });
-    document.getElementById('catalog_product').innerHTML = productsHTML;
+async function pay() {
+  try {
+    const productList = await (
+      await fetch("/api/pay", {
+        method: "post",
+        body: JSON.stringify(carrito),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    ).json();
+  } catch {
+    window.alert("Sin stock");
+  }
+
+  carrito = [];
+  total = 0;
+  await fetchProducts();
+  document.getElementById("checkout").innerHTML = `Pagar $${total}`;
 }
 
-/*===== FETCH =====*/
-window.onload = async() => {
-    const productlist = await (await fetch("/api/products")).json();
-    console.log(productlist);
-    displayProducts(productlist);
+//-----
+function displayProducts() {
+  let productsHTML = "";
+  productList.forEach((p) => {
+    let buttonHTML = `<a class="button-light" onclick="add(${p.id}, ${p.price})">Agregar <i class='bx bxs-arrow-from-left button-icon'></i></a>`;
+
+    if (p.stock <= 0) {
+      buttonHTML = `<a class="button-light-disabled" onclick="add(${p.id}, ${p.price})">SIN STOCK</a>`;
+    }
+
+    productsHTML += `<article class="sneaker">
+        <img src="${p.image}" alt="" class="sneaker_img">
+        <span class="sneaker_name">${p.name}</span>
+        <span class="sneaker_price">$${p.price}</span>
+        <a class="button-light" onclick="add(${p.id}, ${p.price})">Agregar <i class='bx bxs-arrow-from-left button-icon'></i></a>
+    </article>`;
+  });
+  document.getElementById("page-content").innerHTML = productsHTML;
 }
+
+async function fetchProducts() {
+  productList = await (await fetch("/api/products")).json();
+  displayProducts();
+}
+
+window.onload = async () => {
+  await fetchProducts();
+};
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const products = [
   {
@@ -327,16 +331,28 @@ const products = [
     size: 42,
     stock: 5,
   },
-
-
 ];
 
 app.get("/api/products", (req, res) => {
   res.send(products);
 });
 
-app.use("/", express.static("frontEnd"));
+app.post("/api/pay", (req, res) => {
+  const ids = req.body;
+  const procutsCopy = products.map((p) => ({ ...p }));
+  ids.forEach((id) => {
+    const product = procutsCopy.find((p) => p.id === id);
+    if (product.stock > 0) {
+      product.stock--;
+    } else {
+      throw "Sin stock";
+    }
+  });
+  products = procutsCopy;
+  res.send(products);
+});
 
+app.use("/", express.static("frontEnd"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
